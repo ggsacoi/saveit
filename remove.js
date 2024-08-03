630d9aabfe1195b66cb62e1ac4e81e044e73244e
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2024-08-03 12:21:21 +0200

    ecomm

--- remove.js
+++ remove.js
@@ -1,13 +1,21 @@
-const remover = () => {
+addEventListener('DOMContentLoaded', () => {
     const section = document.querySelector('section');
     const menu = document.querySelector(".options");
     const checker = document.getElementById("see");
-    const scrollAmount = window.innerWidth;
+    const span = document.querySelector('.boxdevices');
+    const buybtn = document.getElementById('container');
+    const popup = document.querySelector(".contact-container");
 section.addEventListener("click", () => {
     if (checker.checked) {
         checker.checked = false;
     }
 });
-};
-
-window.addEventListener("load", remover);
+span.addEventListener("click", (event) => {
+  event.stopPropagation();
+  window.open("c'estbien.html", "_top");
+});
+buybtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  window.open("c'estbien.html", "_top");
+});
+});
