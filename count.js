f44c5929fffdc051d7b256896b905810036d8d7c
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2024-08-14 18:30:18 +0200

    ecomm

--- count.js
+++ count.js
@@ -4,24 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let nayebité = localStorage.getItem('text');
     nasilisi.textContent = nayebité;
 
-    let baelokonioso = [
-        { element: document.getElementById('produit1'), url: "egain.html" },
-        { element: document.getElementById('produit2'), url: "egain2.html" },
-        { element: document.getElementById('produit3'), url: "egain3.html" },
-        { element: document.getElementById('produit4'), url: "egain4.html" },
-        { element: document.getElementById('produit5'), url: "egain5.html" },
-        { element: document.getElementById('produit6'), url: "egain6.html" },
-        { element: document.getElementById('produit7'), url: "egain7.html" },
-        { element: document.getElementById('produit8'), url: "egain8.html" },
-        { element: document.getElementById('produit9'), url: "egain9.html" },
-        { element: document.getElementById('produit10'), url: "egain10.html" },
-    ];
-
-    baelokonioso.forEach(({ element, url }) => {
-        if (element) {
-            element.addEventListener('click', () => {
-                window.open(url, '_top');
-            });
-        }
-      });
+    const span = document.querySelector('.boxstore');
+    span.addEventListener('click', (event) => {
+        window.open("store.html", "_top");
+    });
 });
